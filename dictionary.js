57bfba40093fd03d70d42a21c4881bd45bcb06d5
henrycaron <57197754+henrycaron@users.noreply.github.com>
2020-03-03 13:37:36 -0500

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,24 @@
+function Watch(name, price, description, size, image) {
+  this.name = name;
+  this.price = price;
+  this.description = description;
+  this.size = size;
+  this.image = image;
+}
+
+var 1 = new Watch('Fezziwig', 1379.99, "This black beauty was forged in the fires of Mount Doom. Enjoy its deep black felt backing in contrast to its silver accents.", 41, /watches/1.png);
+var 2 = new Watch();
+var 3 = new Watch();
+var 4 = new Watch();
+var 5 = new Watch();
+var 6 = new Watch();
+var 7 = new Watch();
+var 8 = new Watch();
+var 9 = new Watch();
+var 10 = new Watch();
+var 11 = new Watch();
+var 12 = new Watch();
+var 13 = new Watch();
+var 14 = new Watch();
+var 15 = new Watch();
+var 16 = new Watch();
